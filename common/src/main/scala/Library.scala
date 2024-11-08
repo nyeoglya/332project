827fff2d7aee98917fe300032e3a3d7267f9a2ef
zlfn <ung@zlfn.space>
2024-11-09 02:18:30 +0800

    test: add fundamental prototype functions

--- common/src/main/scala/Library.scala
+++ common/src/main/scala/Library.scala
@@ -1,3 +1,9 @@
 package common
 
 final case class Entity(head: String, body: String)
+
+class Partition(pivots: List[String]) {
+  def getEntityRangeIterator(): Iterator[EntityRange] = ???
+}
+
+case class EntityRange(left: String, right: String)
--- worker/src/main/scala/Service.scala
+++ worker/src/main/scala/Service.scala
@@ -1,10 +1,16 @@
 import common.Entity
+import common.Partition
 
 import zio.ZIO
-import zio.stream.ZStream
+import zio.stream.Stream
 
-object Server {
-  def getSampleStream(): ZStream[Any, Exception, Entity] = ???
-  def startShuffling(): ZIO[Any, Exception, Unit] = ???
-  def getPartialData(start: String, end: String): ZStream[Any, Exception, Entity] = ???
+object PrototypeService {
+  def getSampleStreamFromFile(data: Stream[Exception, Entity]): Stream[Exception, Entity] = ???
+
+  def getPartialEntityStreamFromIndex(
+    index: Int, 
+    partition: Partition, 
+    data: Stream[Exception, Entity]): Stream[Exception, Entity] = ???
+
+  def getMergyStream(source: List[Stream[Exception, Entity]]): Stream[Exception, Entity] = ???
 }
