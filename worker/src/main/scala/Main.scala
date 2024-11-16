bf471d580588bbc20414ecb80f17c8f5c5772b4a
zlfn <ung@zlfn.space>
2024-11-16 23:49:42 +0900

    refactor(grpc): refactor App to ZIOApp

--- worker/src/main/scala/Main.scala
+++ worker/src/main/scala/Main.scala
@@ -5,28 +5,39 @@ import scalapb.zio_grpc.{ServerMain, ServiceList}
 import io.grpc.StatusException
 import proto.common.{SampleRequest, SampleResponse}
 import proto.common.ZioCommon.WorkerService
+import zio.ZIOAppDefault
+import zio.{Scope, ZIO, ZIOAppArgs}
+import io.grpc.ServerBuilder
+import io.grpc.protobuf.services.ProtoReflectionService
+import zio.ZLayer
+import scalapb.zio_grpc
 
 class Config(args: Seq[String]) extends ScallopConf(args) {
-  val masterAddress = trailArg[String](required = true, descr = "Master address (e.g., 192.168.0.1:8080)")
   val inputDirectories = opt[List[String]](name = "I", descr = "Input directories", default = Some(List()))
   val outputDirectory = opt[String](name = "O", descr = "Output directory", default = Some(""))
   verify()
 }
 
-object Main extends App {
-  val config = new Config(args)
-  println(s"Master Address: ${config.masterAddress()}")
-  config.inputDirectories.toOption.foreach(dirs => println(s"Input Directories: ${dirs.mkString(", ")}"))
-  config.outputDirectory.toOption.foreach(dir => println(s"Output Directory: $dir"))
+object Main extends ZIOAppDefault {
+  def port: Int = 8980
 
-  Server.run
+  def run: ZIO[Environment with ZIOAppArgs with Scope,Any,Any] = for {
+    args <- getArgs
+    config = new Config(args)
+    _ = config.inputDirectories.toOption.foreach(dirs => println(s"Input Directories: ${dirs.mkString(", ")}"))
+    _ = config.outputDirectory.toOption.foreach(dir => println(s"Output Directory: $dir"))
+    _ <- zio.Console.printLine(s"Worker is running on port ${port}. Press Ctrl-C to stop.")
+    result <- serverLive.launch.exitCode
+  } yield result
+
+  def builder = ServerBuilder
+    .forPort(port)
+    .addService(ProtoReflectionService.newInstance())
+
+  def serverLive: ZLayer[Any, Throwable, zio_grpc.Server] =
+    zio_grpc.ServerLayer.fromServiceList(builder, ServiceList.add(ServiceImpl))
 }
 
 object ServiceImpl extends WorkerService {
   def getSamples(request: SampleRequest): zio.stream.Stream[StatusException,SampleResponse] = ???
-}
-
-object Server extends ServerMain {
-  def services = ServiceList.add(ServiceImpl)
-  override def port: Int = 8980
 }
