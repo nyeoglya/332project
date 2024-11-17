c11f61da1755401ad008a6f174d011b5b0e39ec0
hyunseong <hyunseong@postech.ac.kr>
2024-11-18 03:16:49 +0900

    feat(master, proto): fix bugs

--- master/src/main/scala/Main.scala
+++ master/src/main/scala/Main.scala
@@ -11,6 +11,7 @@ import io.grpc.ManagedChannelBuilder
 import proto.common.ZioCommon.WorkerServiceClient
 import proto.common.Entity
 import proto.common.Pivots
+import proto.common.ShuffleRequest
 import io.grpc.ServerBuilder
 import io.grpc.protobuf.services.ProtoReflectionService
 import proto.common.ZioCommon.MasterService
@@ -53,4 +54,81 @@ object Main extends ZIOAppDefault {
       ZIO.succeed(WorkerDataResponse())
     }
   }
+}
+
+class MasterLogic(config: Config) {
+  case class WorkerClient(val client: Layer[Throwable, WorkerServiceClient], val size: BigInt)
+  
+  var workerIPList: List[String] = List()
+  var clients: List[WorkerClient] = List()
+
+  lazy val offset = ???
+
+  /** Add new client connection to MasterLogic
+    *
+    * @param clientAddress address of client
+    */
+  def addClient(clientAddress: String, clientSize: BigInt) {
+    println(s"New client[${clients.size}] attached: ${clientAddress}, Size: ${clientSize} Bytes")
+    val address = AddressParser.parse(clientAddress).get
+    clients = clients :+ WorkerClient(WorkerServiceClient.live(
+      ZManagedChannel(
+        ManagedChannelBuilder.forAddress(address._1, address._2).usePlaintext()
+      )
+    ), clientSize)
+    workerIPList = workerIPList :+ clientAddress
+    if (clients.size == config.workerNum.toOption.get) this.run()
+  }
+
+  def run() = {
+    // TODO: Collect samples from workers and select pivot
+    // val partition = selectPivots(collectSamples())
+    // TODO: Iterate clientLayers and send partition datas
+    val pivotCandicateList: ZIO[Any, Throwable, List[Pivots]] = ZIO.foreachPar(clients.map(_.client)) { layer =>
+      collectSample(layer).provideLayer(layer)
+    }
+
+    val selectedPivots = selectPivots(pivotCandicateList)
+
+    // TODO: selectedPivots를 모든 Worker에 전송한다.
+  }
+  
+  def collectSample(client: Layer[Throwable, WorkerServiceClient]): ZIO[WorkerServiceClient, Throwable, Pivots] =
+    ZIO.serviceWithZIO[WorkerServiceClient] { workerServiceClient =>
+      workerServiceClient.getSamples(SampleRequest(offset))
+  }
+
+  def selectPivots(pivotCandicateZIOList: ZIO[Any, Throwable, List[Pivots]]): ZIO[Any, Throwable, Pivots] = {
+    val pivotCandicateList: ZIO[Any, Throwable, List[String]] = pivotCandicateZIOList.map { pivots =>
+      pivots.flatMap(_.pivots)
+    }
+
+    val pivotCandicateListSize: ZIO[Any, Throwable, BigInt] = pivotCandicateList.map(_.size)
+    val totalDataSize: BigInt = clients.map(_.size).sum
+
+    val pivotIndices: ZIO[Any, Throwable, List[Int]] = for {
+      candidateListSize <- pivotCandicateListSize
+      result <- ZIO.succeed(
+        clients.map(_.size).scanLeft(0) { (acc, workerSize) =>
+          acc + (candidateListSize * (workerSize / totalDataSize)).toInt
+        }.tail
+      )
+    } yield result
+
+    for {
+      pivotList <- pivotCandicateList
+      indices <- pivotIndices
+      distinctList = indices.map(idx => pivotList(idx)).distinct
+    } yield Pivots(pivots = distinctList)
+  }
+
+  def sendPartitionToWorker(client: Layer[Throwable, WorkerServiceClient],
+    pivots: ZIO[Any, Throwable, Pivots]): ZIO[Any, Throwable, Unit] = for {
+    pivotsData <- pivots
+    workerServiceClient <- client.build
+    shuffleRequest = ShuffleRequest(pivots = Some(pivotsData), workerAddresses = ???)
+      _ <- ZIO.fromFuture { implicit ec =>
+      workerServiceClient.startShuffle(shuffleRequest).map(_ => ())
+    }.mapError(e => new RuntimeException(s"Failed to send ShuffleRequest: ${e.getMessage}"))
+  } yield ()
 }
