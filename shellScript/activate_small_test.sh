5cd39711d2b56a88990c2f0d1474d407e4357826
hyunseong <hyunseong@postech.ac.kr>
2024-12-01 21:57:29 +0900

    fix(shell): change shell
    Former-commit-id: 7ad10556b2bc16ec2e7067a587c1f4defbfb8195

--- shellScript/activate_small_test.sh
+++ shellScript/activate_small_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-workers=("2.2.2.101" "2.2.2.102" "2.2.2.103" "2.2.2.104" "2.2.2.105" "2.2.2.106" "2.2.2.107" "2.2.2.108" "2.2.2.109" "2.2.2.110")
+workers=("2.2.2.101" "2.2.2.102" "2.2.2.104" "2.2.2.105" "2.2.2.106" "2.2.2.107" "2.2.2.108" "2.2.2.109" "2.2.2.110" "2.2.2.111" )
 
 worker_num=10
 input_dir="/home/green/dataset/small_input0"
 output_dir="/home/green/dataset/small_output"
+master_ip="2.2.2.254:50050"
 
 echo "Activate master..."
 
@@ -12,7 +13,7 @@ master $worker_num
 
 for worker in "${workers[@]}"; do
 	echo "Activate $worker..."
-	ssh green@$worker "worker 2.2.2.254 -I $input_dir -O $output_dir"
+	ssh green@$worker "worker $master_ip -I $input_dir -O $output_dir"
 done
 
 echo "Activate master & 10 workers"
