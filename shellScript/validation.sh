d1b31fa392345b1c62448568eddeea8b8ec397fb
hyunseong <hyunseong@postech.ac.kr>
2024-12-05 09:42:08 +0900

    fix(shell): fix validation shell
    Former-commit-id: fcbd1fb6209d1a8472a64137a2939f09c39ada86

--- shellScript/validation.sh
+++ shellScript/validation.sh
@@ -2,24 +2,26 @@
 
 workers=("2.2.2.101" "2.2.2.102" "2.2.2.104" "2.2.2.105" "2.2.2.106" "2.2.2.107" "2.2.2.108" "2.2.2.109" "2.2.2.110" "2.2.2.111")
 home_folder="/home/green"
-remote_folder="$home_folder/dataset/large_output"
+remote_folder="$home_folder/dataset/big_output"
 master_local_folder="$home_folder/validation"
-output_file="$home_folder/validation/merged_file"
+output_file="$home_folder/validation_merged.txt"
 
 set -e
+rm -r "$master_local_folder" || true
 mkdir -p "$master_local_folder"
 
 for worker in "${workers[@]}"; do
-    echo "> Processing $worker..."
+    echo ">> Processing $worker..."
+
+    ssh "$worker" "find \"$remote_folder\" -name 'headtail_*' -exec rm {} \;"
 
     # valsort & send results
     ssh "$worker" "
-        for FILE in $remote_folder/*; do 
-            ./valsort \"\$FILE\" > \"$home_folder/${worker}_valsort_result.txt\"
+        remote_home=\"$home_folder\"
+        for FILE in $remote_folder/*; do
+            ./valsort \"\$FILE\"
         done
     "
-
-    # scp "$worker:$home_folder/${worker}_valsort_result.txt" "$master_local_folder/"
     
     # extract head & tail from files
     ssh "$worker" "
@@ -33,16 +35,16 @@ for worker in "${workers[@]}"; do
     # send head & tail to master
     scp "$worker:$remote_folder/headtail_*" "$master_local_folder/"
     
-    echo "> successfully move headtail from $worker."
+    echo " "
 done
 
-# merge files
-find "$master_local_folder" -type f | sort | while read -r FILE; do
-  echo "> merge $master_local_folder..."
-  cat "$FILE" >> "$output_file"
-  echo -e "\n" >> "$output_file" # add \n between files
+> "$output_file"
+for file in $(ls "$master_local_folder" | sort); do
+    if [ -f "$master_local_folder/$file" ]; then
+        cat "$master_local_folder/$file" >> "$output_file"
+    fi
 done
 
-valsort $output_file > $home_folder/result.txt # final valsort result file
+"$home_folder/valsort" "$output_file" # final valsort result file
 
-echo "Complete work from worker. Results were saved in $master_local_folder."
+echo ">> complete validation."
